67b951217951715442a2507ec47e8a1f1c7fb16b
Kai Guo <243352548@st.usst.edu.cn>
2025-12-18 17:16:11 +0800

    Add ProDU method

---
+++ src/methods/net/ProDU.py
@@ -0,0 +1,379 @@
+import os.path as osp
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from src.utils import loss,prompt_tuning,IID_losses
+from src.models import network,shot_model
+from sklearn.metrics import confusion_matrix
+import clip
+from clip.custom_clip import get_coop
+from src.utils.utils import *
+from src.data.datasets.data_loading import get_test_loader, get_test_loader_aug, get_test_loader_p
+from src.data.datasets.imagenet_subsets import IMAGENET_A_MASK, IMAGENET_R_MASK,IMAGENET_V_MASK
+from src.models.model import *
+from data.imagnet_prompts import imagenet_classes
+
+def lr_scheduler(optimizer, iter_num, max_iter, gamma=10, power=0.75):
+    decay = (1 + gamma * iter_num / max_iter) ** (-power)
+    # decay = 1
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = param_group['lr0'] * decay
+        param_group['weight_decay'] = 1e-3
+        param_group['momentum'] = 0.9
+        param_group['nesterov'] = True
+    return optimizer
+
+def cal_acc(loader, base_model, flag=False):
+    start_test = True
+    with torch.no_grad():
+        iter_test = iter(loader)
+        for i in range(len(loader)):
+            data = next(iter_test)
+            inputs = data[0]
+            labels = data[2]
+            inputs = inputs.cuda()
+            outputs = base_model(inputs)
+            if start_test:
+                all_output = outputs.float().cpu()
+                all_label = labels.float()
+                start_test = False
+            else:
+                all_output = torch.cat((all_output, outputs.float().cpu()), 0)
+                all_label = torch.cat((all_label, labels.float()), 0)
+    _, predict = torch.max(all_output, 1)
+    accuracy = torch.sum(torch.squeeze(predict).float() == all_label).item() / float(all_label.size()[0])
+    mean_ent = torch.mean(loss.Entropy(nn.Softmax(dim=1)(all_output))).cpu().data.item()
+
+    if flag:
+        matrix = confusion_matrix(all_label, torch.squeeze(predict).float())
+        acc = matrix.diagonal()/matrix.sum(axis=1) * 100
+        aacc = acc.mean()
+        aa = [str(np.round(i, 2)) for i in acc]
+        acc = ' '.join(aa)
+        return aacc, acc
+    else:
+        return accuracy*100, mean_ent
+
+
+def op_copy(optimizer):
+    for param_group in optimizer.param_groups:
+        param_group['lr0'] = param_group['lr']
+    return optimizer
+
+def MI_perturbation(cfg, theta_v, output_t, P_EPSILON, sign='max'):
+    
+    if sign not in ('max', 'min'):
+        raise ValueError('The value of sign must be \'max\' or \'min\'.')
+
+    theta_v = theta_v.detach()
+    output_t = output_t.detach()
+
+    p_t = F.softmax(output_t, dim=1)
+
+    delta = torch.zeros_like(theta_v, requires_grad=True)
+    optimizer = torch.optim.Adam([delta], lr=cfg.ProDU.DELTA_LR)
+    for _ in range(cfg.ProDU.P_STEPS):
+        optimizer.zero_grad()
+
+        perturbed_logits = theta_v + delta
+        p_v = F.softmax(perturbed_logits, dim=1)
+
+        mi_loss = IID_losses.IID_loss(p_t, p_v)
+        if sign == 'min':
+            mi_loss *= -1
+
+        overflow = torch.clamp(torch.abs(delta) - P_EPSILON, min=0.0)
+        # 1.0 for best
+        reg_loss =  cfg.ProDU.INNER * torch.sum(overflow)
+
+        total_loss = reg_loss + mi_loss
+
+        total_loss.backward()
+        optimizer.step()
+
+    # with torch.no_grad():
+    #     delta.data = delta.data.clamp(-args.p_epsilon, args.p_epsilon)
+    
+    perturbed_logits_v = (theta_v + delta).detach()
+    return perturbed_logits_v
+
+def train_target(cfg):
+    text_inputs = clip_pre_text(cfg)
+    
+    if 'image' in cfg.SETTING.DATASET:
+        if cfg.MODEL.ARCH[0:3] == 'res':
+            netF = network.ResBase(res_name=cfg.MODEL.ARCH)
+        elif cfg.MODEL.ARCH[0:3] == 'vgg':
+            netF = network.VGGBase(vgg_name=cfg.MODEL.ARCH)
+        netC = network.Net2(2048,1000)
+        base_model = get_model(cfg, cfg.class_num)
+        netC.linear.load_state_dict(base_model.model.fc.state_dict())
+        del base_model
+        Shot_model = shot_model.OfficeHome_Shot(netF,netC)
+        base_model = Shot_model
+        if cfg.SETTING.DATASET == "imagenet_a":
+            base_model = ImageNetXWrapper(base_model, IMAGENET_A_MASK)
+        elif cfg.SETTING.DATASET == "imagenet_r":
+            base_model = ImageNetXWrapper(base_model, IMAGENET_R_MASK)
+        elif cfg.SETTING.DATASET == "imagenet_d109":
+            base_model = ImageNetXWrapper(base_model, IMAGENET_D109_MASK)
+        elif cfg.SETTING.DATASET == "imagenet_v":
+            base_model = ImageNetXWrapper(base_model, IMAGENET_V_MASK)
+    else :
+        base_model = get_model(cfg, cfg.class_num)
+        
+    base_model = base_model.cuda()
+
+    model = get_coop(cfg.ProDU.ARCH, cfg.SETTING.DATASET, int(cfg.GPU_ID), cfg.ProDU.N_CTX, cfg.ProDU.CTX_INIT)
+    
+    for name, param in model.named_parameters():
+        if "prompt_learner" not in name:
+            param.requires_grad_(False)
+
+    if cfg.MODEL.ARCH[0:3] == 'res':
+        netF = network.ResBase(res_name=cfg.MODEL.ARCH).cuda()
+    elif cfg.MODEL.ARCH[0:3] == 'vgg':
+        netF = network.VGGBase(vgg_name=cfg.MODEL.ARCH).cuda()  
+
+    param_group = []
+    param_group_ib = []
+    for k, v in base_model.named_parameters():
+        # if 'netC' in k or 'fc' in k:
+        #     v.requires_grad = False
+        # else:
+            param_group += [{'params': v, 'lr': cfg.OPTIM.LR}]
+
+    for k, v in model.prompt_learner.named_parameters():
+        if(v.requires_grad == True):
+            param_group_ib += [{'params': v, 'lr': cfg.OPTIM.LR * cfg.OPTIM.LR_DECAY1}]
+
+    optimizer = optim.SGD(param_group)
+    optimizer = op_copy(optimizer)
+    optimizer_ib = optim.SGD(param_group_ib)
+    optimizer_ib = op_copy(optimizer_ib)
+    optim_state = deepcopy(optimizer_ib.state_dict())
+    classnames = cfg.classname
+    model.reset_classnames(classnames, cfg.ProDU.ARCH)
+
+    cfg.ADAPTATION = 'tent'
+    domain_name = cfg.domain[cfg.SETTING.T]
+
+    target_data_loader = get_test_loader_p(setting='reset_each_shift',
+                                        adaptation=cfg.ADAPTATION,
+                                        dataset_name=cfg.SETTING.DATASET,
+                                        root_dir=cfg.DATA_DIR,
+                                        domain_name=domain_name,
+                                        severity=5,
+                                        num_examples=-1,
+                                        rng_seed=cfg.SETTING.SEED,
+                                        domain_names_all=cfg.domain,
+                                        alpha_dirichlet=0.0,
+                                        batch_size=cfg.TEST.BATCH_SIZE,
+                                        shuffle=True,
+                                        workers=cfg.NUM_WORKERS,trans = image_train())
+
+
+    test_data_loader = get_test_loader_p(setting='reset_each_shift',
+                                    adaptation=cfg.ADAPTATION,
+                                    dataset_name=cfg.SETTING.DATASET,
+                                    root_dir=cfg.DATA_DIR,
+                                    domain_name=domain_name,
+                                    severity=5,
+                                    num_examples=-1,
+                                    rng_seed=cfg.SETTING.SEED,
+                                    domain_names_all=cfg.domain,
+                                    alpha_dirichlet=0.0,
+                                    batch_size=cfg.TEST.BATCH_SIZE*3,
+                                    shuffle=False,
+                                    workers=cfg.NUM_WORKERS,trans = image_test())
+    
+    source_data_loader = get_test_loader_p(setting='reset_each_shift',
+                                    adaptation=cfg.ADAPTATION,
+                                    dataset_name=cfg.SETTING.DATASET,
+                                    root_dir=cfg.DATA_DIR,
+                                    domain_name=domain_name,
+                                    severity=5,
+                                    num_examples=-1,
+                                    rng_seed=cfg.SETTING.SEED,
+                                    domain_names_all=cfg.domain,
+                                    alpha_dirichlet=0.0,
+                                    batch_size=cfg.TEST.BATCH_SIZE*3,
+                                    shuffle=False,
+                                    workers=cfg.NUM_WORKERS,trans = image_test())
+
+
+    max_iter = cfg.TEST.MAX_EPOCH * len(target_data_loader)
+    batch_num =  max_iter // cfg.TEST.MAX_EPOCH
+    interval_iter = max_iter // cfg.TEST.INTERVAL
+    iter_num = 0
+    num_sample=len(target_data_loader.dataset)
+    logtis_bank = torch.randn(num_sample, cfg.class_num).cuda()
+    p_bank = torch.zeros_like(logtis_bank)
+
+    with torch.no_grad():
+        iter_test = iter(source_data_loader)
+        for i in range(len(source_data_loader)):
+            data = next(iter_test)
+            inputs = data[0]
+            indx=data[-1]
+            inputs = inputs.cuda()
+            outputs = base_model(inputs)
+            logtis_bank[indx] = outputs.detach().clone()
+            p_bank[indx] = F.softmax(outputs.detach().clone(), dim=1)
+
+    while iter_num < max_iter:
+        try:
+            inputs_test, inputs_test_augs, target, tar_idx = next(iter_test)
+        except:
+            iter_test = iter(target_data_loader)
+            inputs_test, inputs_test_augs, target, tar_idx = next(iter_test)
+        if inputs_test.size(0) == 1:
+            continue
+
+        inputs_test = inputs_test.cuda()
+        inputs_test_augs = inputs_test_augs.cuda() 
+
+        epoch_num = iter_num // batch_num
+
+        iter_num += 1
+        lr_scheduler(optimizer, iter_num=iter_num, max_iter=max_iter)
+
+
+        outputs_test = base_model(inputs_test)
+        softmax_out = nn.Softmax(dim=1)(outputs_test)
+        with torch.no_grad():
+            outputs_test_new = outputs_test.clone().detach()
+
+        if cfg.ProDU.ARCH == 'RN50':
+            inputs_test_clip = inputs_test_augs
+        else: 
+            inputs_test_clip = inputs_test
+
+        clip_score = test_time_adapt_eval(inputs_test_clip, model, optimizer_ib, optim_state, cfg, outputs_test_new)
+        clip_score = clip_score.float()
+
+        t_entropy = -torch.sum(softmax_out*torch.log(softmax_out+1e-10), dim=1)
+        m_t_entropy = torch.mean(t_entropy)
+
+        with torch.no_grad():
+            new_clip = (outputs_test_new - 0.1*logtis_bank[tar_idx].cuda()) + clip_score.cuda()
+            raw_clip = clip_score.cuda()
+            clip_score_sm = nn.Softmax(dim=1)(new_clip)
+
+            s_entropy = -torch.sum(p_bank[tar_idx]*torch.log(p_bank[tar_idx]+1e-10), dim=1)
+            m_s_entropy = torch.mean(s_entropy)
+            res_entropy = m_t_entropy / (m_s_entropy + cfg.ProDU.EPSILON)
+
+            if iter_num == 1:
+                max_val = res_entropy
+            r_entropy = res_entropy / max_val
+            
+            W = r_entropy
+        
+        P_EPSILON =  cfg.ProDU.L * (1 - torch.sigmoid(W/cfg.ProDU.R))
+        # print(args.w) 
+        clr_new_clip = MI_perturbation(cfg, raw_clip, outputs_test, P_EPSILON, sign='max')
+
+        max_probs, _ = torch.max(clip_score_sm, dim=1)
+        confident_idx = torch.where(max_probs > cfg.ProDU.THRESHOLD+0*(iter_num/max_iter))[0]
+        
+        _,clr_clip_index_new = torch.max(clr_new_clip, 1)
+        clr_clip_score_sm = nn.Softmax(dim=1)(clr_new_clip)
+        _,clip_index_new = torch.max(new_clip, 1)
+        clip_score_sm = nn.Softmax(dim=1)(new_clip)
+
+        iic_loss = IID_losses.IID_loss(softmax_out, clip_score_sm)
+        p_iic_loss = IID_losses.IID_loss(softmax_out, clr_clip_score_sm)
+        iic_loss += cfg.ProDU.OUTER*p_iic_loss
+
+        classifier_loss = cfg.ProDU.IIC_PAR * iic_loss
+        msoftmax = softmax_out.mean(dim=0)
+
+        if  cfg.SETTING.DATASET=='office':
+            gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + cfg.ProDU.EPSILON))
+            classifier_loss = classifier_loss - 1.0 * gentropy_loss
+        if  cfg.SETTING.DATASET=='office-home':
+            gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + cfg.ProDU.EPSILON))
+            classifier_loss = classifier_loss - 1.0 * gentropy_loss
+        if  cfg.SETTING.DATASET=='VISDA-C':
+            gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + cfg.ProDU.EPSILON))
+            classifier_loss = classifier_loss - 0 * gentropy_loss
+        if  cfg.SETTING.DATASET=='domainnet126':
+            gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + cfg.ProDU.EPSILON))
+            classifier_loss = classifier_loss - 0.5 * gentropy_loss
+        pred = clip_index_new
+        entropy_loss = nn.CrossEntropyLoss()(outputs_test[confident_idx], pred[confident_idx])
+        classifier_loss =  cfg.ProDU.GENT_PAR*entropy_loss + classifier_loss
+
+        optimizer.zero_grad()
+        classifier_loss.backward()
+        optimizer.step()
+
+        if iter_num % interval_iter == 0 or iter_num == max_iter:
+            base_model.eval()
+            if cfg.SETTING.DATASET=='VISDA-C':
+                acc_s_te, acc_list = cal_acc(test_data_loader, base_model, True)
+                log_str = 'Task: {}, Iter:{}/{}; Accuracy = {:.2f}%;loss ={}'.format(cfg.name, iter_num, max_iter, acc_s_te,classifier_loss) + '\n' + acc_list
+            else:
+                acc_s_te, _ = cal_acc(test_data_loader, base_model, False)
+                log_str = 'Task: {}, Iter:{}/{}; Accuracy = {:.2f}%;loss ={}'.format(cfg.name, iter_num, max_iter, acc_s_te,classifier_loss)
+
+            logger.info(log_str)
+            base_model.train()
+        
+    if cfg.ISSAVE:   
+        torch.save(base_model.state_dict(), osp.join(cfg.output_dir, "target" + cfg.savename + ".pt"))
+
+    return base_model
+
+def test_time_tuning(model, inputs, optimizer, cfg, target_output):
+    target_output = target_output.cuda()
+    target_output =  nn.Softmax(dim=1)(target_output)
+    for j in range(cfg.ProDU.TTA_STEPS):
+        with torch.amp.autocast('cuda'):
+            output_logits,_ = model(inputs)             
+            output = nn.Softmax(dim=1)(output_logits)
+            iic_loss = IID_losses.IID_loss(output, target_output)
+        optimizer.zero_grad()
+        iic_loss.backward()
+        optimizer.step()
+    return output
+
+def test_time_adapt_eval(input, model, optimizer, optim_state, cfg, target_output):
+    optimizer.load_state_dict(optim_state)
+    output = test_time_tuning(model, input, optimizer, cfg, target_output)
+    with torch.no_grad():
+        with torch.amp.autocast('cuda'):
+            model.eval()
+            output,_ = model(input)
+    return output
+
+
+def clip_pre_text(cfg):
+    List_rd = []
+    if 'image' in cfg.SETTING.DATASET:
+        classnames_all = imagenet_classes
+        classnames = []
+        if cfg.SETTING.DATASET.split('_')[-1] in ['a','r','v']:
+            label_mask = eval("imagenet_{}_mask".format(cfg.SETTING.DATASET.split('_')[-1]))
+            if 'r' in cfg.SETTING.DATASET:
+                for i, m in enumerate(label_mask):
+                    if m:
+                        classnames.append(classnames_all[i])
+            else:
+                classnames = [classnames_all[i] for i in label_mask]
+        else:
+            classnames = classnames_all
+    else:
+        with open(cfg.name_file) as f:
+            for line in f:
+                List_rd.extend([i for i in line.split()])
+        f.close()
+        classnames = List_rd
+    classnames = [name.replace("_", " ") for name in classnames]
+    cfg.classname = classnames
+    prompt_prefix = cfg.ProDU.CTX_INIT.replace("_"," ")
+    prompts = [prompt_prefix + " " + name + "." for name in classnames]
+    tokenized_prompts = torch.cat([clip.tokenize(p) for p in prompts]).cuda()
+    return tokenized_prompts
