2b5ff08561028a943a406ff6ae31248a6f738c38
Kai Guo <243352548@st.usst.edu.cn>
2025-12-18 17:12:16 +0800

    Add ProDU method

--- image_target_of_oh_vs.py
+++ image_target_of_oh_vs.py
@@ -10,9 +10,10 @@ import src.methods.oh.cowa as COWA
 import src.methods.oh.gkd as GKD
 import src.methods.oh.tpds as TPDS
 import src.methods.oh.nrc_vs as Nrc_vs
-import src.methods.oh.CausalDA as CausalDA
+import src.methods.oh.lcfd as LCFD
 import src.methods.oh.difo as DIFO
 import src.methods.oh.ProDe as ProDe
+import src.methods.oh.ProDU as ProDU
 import src.methods.oh.tsd as TSD
 import src.methods.oh.difo_50 as DIFO_50
 
@@ -75,9 +76,9 @@ if __name__ == "__main__":
         print("using nrc method")
         acc = Nrc_vs.train_target(cfg)
 
-    elif cfg.MODEL.METHOD == "CausalDA":
-        print("using CausalDA method")
-        acc = CausalDA.train_target(cfg)
+    elif cfg.MODEL.METHOD == "lcfd":
+        print("using lcfd method")
+        acc = LCFD.train_target(cfg)
 
     elif cfg.MODEL.METHOD == "difo":
         print("using difo method")
@@ -89,10 +90,14 @@ if __name__ == "__main__":
     elif cfg.MODEL.METHOD == "tsd":
         print("using tsd method")
         acc = TSD.train_target(cfg)
+
     elif cfg.MODEL.METHOD == "ProDe":
         print("using ProDe method")
         acc = ProDe.train_target(cfg)
 
+    elif cfg.MODEL.METHOD == "ProDU":
+        print("using ProDU method")
+        acc = ProDU.train_target(cfg)
 
     elif cfg.MODEL.METHOD == "plue":
         print("using plue method")
